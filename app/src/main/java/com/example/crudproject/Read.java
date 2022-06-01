4887fee5063ae08adf6745a79dcdde31576b0c7b
Oliver Marie <oliver.j.a.marie@gmail.com>
2022-06-01 18:29:46 +0200

    Update Read class

--- app/src/main/java/com/example/crudproject/Read.java
+++ app/src/main/java/com/example/crudproject/Read.java
@@ -1,24 +1,45 @@
 package com.example.crudproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
 public class Read extends AppCompatActivity {
+
+    private static String TAG = "READER";
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read);
-        Bundle extras = getIntent().getExtras();
 
-        if (extras != null){
-            String values = extras.getString("key");
-            TextView txtView = findViewById(R.id.IDTxt);
-            txtView.setText(values);
-        }
+        recyclerView = findViewById(R.id.recycleView);
 
-        String response = getString(R.string.readResponseJSON);
+        Intent intent = getIntent();
+        String response = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        Log.d(TAG,response);
+
+        Adapter adapter = new Adapter(this,response);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
     }
+
+
 }
